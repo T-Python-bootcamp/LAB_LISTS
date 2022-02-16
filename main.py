862ec404971dbb20139ad1b2949621919cba7161
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2022-02-16 13:40:46 +0300

    finished lab

---
+++ main.py
@@ -0,0 +1,46 @@
+lst = [3, 4, 5]
+"""
+x = 0
+for n in lst:
+    x += n
+    print(x)
+ """  
+#  ---------------------- 
+
+"""
+max = lst[0]
+for i in lst:
+    if i > max:
+        max = i 
+        print(max)   
+        
+""" 
+#  ---------------------- 
+
+"""
+odds = []
+odds = [item for item in range(1200, 2000, 125) if item%2 ]
+print(odds)
+"""
+#  ---------------------- 
+
+"""
+tup = ("A", "B", "C", "A")
+for i in tup:
+    if tup.count(i) > 1:
+        print(tup.index(i))
+"""
+#  ---------------------- 
+
+def dubs():
+    tup = ("A", "B", "C", "A")
+    lst = list(tup)
+    for i in tup:
+        if tup.count(i) > 1:
+            print(i)
+            lst.remove(i)
+            print(lst)
+            tup = tuple(lst)
+            print(tup)
+            
+dubs()
