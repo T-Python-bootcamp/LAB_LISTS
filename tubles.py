03c915e63a7dac812161b0f4f2bcf13502737f91
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2022-02-16 14:10:08 +0300

    add

--- tubles.py
+++ tubles.py
@@ -1,10 +1,12 @@
+#Q1: Create a tuple with duplicate elements and find the index of the the duplicate item.
 #A1
-def lastIndex(tub):
-    print(tub.index(1,-1))
+def lastIndex(tubl):
+    print(tubl.index(1))
 lastIndex((1,2,5,4,1))
+#Q2: Write a Python program to remove duplicates from a tuple
 #A2
-def removeDubil(tublll):
-    newSet=set(tublll)
-    tublll=newSet
-    print(tublll)
-removeDubil((1,3,5,5))
+def removeDubl(tubl):
+    newSet=set(tubl)
+    tubl=newSet
+    print(tubl)
+removeDubl((1,3,5,5))
