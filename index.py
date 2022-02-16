2e29f0293fd9ca247e5996da2f67f141a2316495
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2022-02-16 13:46:56 +0300

    tuple

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+# LISTS
 # Q1:
 lst = [2,5,11,3.6]
 sumItems=0
@@ -20,7 +21,19 @@ for index in range(len(lst)):
 
 print(largestNum," Is largest number in a list.")
 
-# Q3: Create an odd numbers list from the elements of a range from 1200 to 2000 with steps of 125, using list comprehension.
+# Q3:
 
 oddNumbers = [ oddNum for oddNum in range(1200,2000,125) if not oddNum % 2 == 0  ]
 print(oddNumbers, " Is list to odd numbers between 1200 to 2000 with step 125.")
+
+
+# TUPLES
+### Q1: Create a tuple with duplicate elements and find the index of the the duplicate item.
+tupleDuple = ("W", "A", "L", "A")
+print(tupleDuple.index("A"))
+
+### Q2: Write a Python program to remove duplicates from a tuple
+lstTup = list(tupleDuple)
+lstTup.remove("A")
+lastTupleAfterRemove = tuple(lstTup)
+print(lastTupleAfterRemove)
